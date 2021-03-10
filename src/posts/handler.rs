42f439b52cc1fc690a122a4c80251981a8e86312
Simon Schmitt <sls@sls.wtf>
2021-03-10 08:36:02 +0100

    IT WORKS HAHAHAHAHAH

--- src/posts/handler.rs
+++ src/posts/handler.rs
@@ -5,34 +5,36 @@ use std::env;
 use crate::posts;
 use crate::posts::Post;
 use rocket::http::Status;
-use rocket::response::{status, Failure};
+use rocket::response::status;
+use rocket::outcome::Outcome;
 use rocket_contrib::json::Json;
 
 #[get("/")]
-pub fn list(connection: DbConn) -> Result<Json<Vec<Post>>, Failure> {
+pub fn list(connection: DbConn) -> Result<Json<Vec<Post>>, Status> {
     posts::repository::all(&connection)
         .map(|post| Json(post))
         .map_err(|error| error_status(error))
 }
 
-fn error_status(error: Error) -> Failure {
-    Failure(match error {
+fn error_status(error: Error) -> Status {
+    match error {
         Error::NotFound => Status::NotFound,
         _ => Status::InternalServerError
-    });
+    }
 }
 
 #[get("/<id>")]
-pub fn get(id: i32, connection: DbConn) -> Result<Json<Post>, Failure> {
+pub fn get(id: i32, connection: DbConn) -> Result<Json<Post>, Status> {
     posts::repository::get(id, &connection)
         .map(|post| Json(post))
         .map_err(|error| error_status(error))
 }
 
 #[post("/", format="application/json", data="<post>")]
-pub fn post(post: Json<Post>, connection: DbConn) -> Result<status::Created<Json<Post>>, Failure> {
+pub fn post(post: Json<Post>, connection: DbConn) -> Result<status::Created<Json<Post>>, Status> {
     posts::repository::insert(post.into_inner(), &connection)
         .map(|post| post_created(post))
+        .map_err(|error| error_status(error))
 }
 
 pub fn post_created(post: Post) -> status::Created<Json<Post>> {
@@ -54,14 +56,14 @@ fn port() -> String {
 }
 
 #[put("/<id>", format="application/json", data="<post>")]
-pub fn put(id: i32, post: Json<Post>, connection: DbConn) -> Result<Json<Post>, Failure> {
+pub fn put(id: i32, post: Json<Post>, connection: DbConn) -> Result<Json<Post>, Status> {
     posts::repository::update(id, post.into_inner(), &connection)
         .map(|post| Json(post))
         .map_err(|error| error_status(error))
 }
 
 #[delete("/<id>")]
-pub fn delete(id: i32, connection: DbConn) -> Result<Json<Post>, Failure>{
+pub fn delete(id: i32, connection: DbConn) -> Result<status::NoContent, Status> {
     match posts::repository::get(id, &connection) {
         Ok(_) => posts::repository::delete(id, &connection)
             .map(|_| status::NoContent)
